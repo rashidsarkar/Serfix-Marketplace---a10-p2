256f0e3b5e8bc0143e8542649ad39f6ecaa05ebe
rashidsarkar <rashidrock558@gmail.com>
2023-10-18 00:10:12 +0600

    Design the SectionTitle component layout to create an appealing and distinctive section title.

---
+++ src/Components/BrandCards/BrandCards.jsx
@@ -0,0 +1,5 @@
+function BrandCards() {
+  return <div></div>;
+}
+
+export default BrandCards;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,13 @@
 import Banar from "../Components/Banar";
+import BrandCards from "../Components/BrandCards/BrandCards";
+import SectionTitle from "../Components/SectionTitle";
 
 function Home() {
   return (
     <div className="min-h-screen">
       <Banar></Banar>
+      <SectionTitle title={"FEATURED CAR"}></SectionTitle>
+      <BrandCards></BrandCards>
     </div>
   );
 }
