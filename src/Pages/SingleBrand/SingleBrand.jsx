c1deec7f58585e45bfa5b614a8363c4a1af5592b
rashidsarkar <rashidrock558@gmail.com>
2023-10-18 13:50:09 +0600

    Create Car Information Card Component

--- src/Pages/SingleBrand/SingleBrand.jsx
+++ src/Pages/SingleBrand/SingleBrand.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
@@ -6,8 +6,12 @@ import "swiper/css/navigation";
 import { Pagination, Autoplay, Navigation } from "swiper/modules";
 import { useParams } from "react-router-dom";
 import "./SingleBrand.css";
+import { Box, Rating, Typography } from "@mui/material";
+useState;
 
 function SingleBrand() {
+  const [Ratingvalue, setRatingvalue] = useState(2);
+
   const { brandName } = useParams();
 
   // Replace the sample data with GTR 34 car details
@@ -56,6 +60,48 @@ function SingleBrand() {
           <img src={product.image} className="md:h-[600px] mx-auto" alt="" />
         </SwiperSlide>
       </Swiper>
+
+      <div className="card card-compact my-10 w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src={`https://i.ibb.co/JddJCCC/pexels-ambady-kolazhikkaran-14061321.jpg`}
+            alt={name}
+            className="rounded-t-lg"
+          />
+        </figure>
+        <div className="card-body p-4">
+          <h2 className="card-title text-xl font-semibold">Toyota GR-456</h2>
+          <p className="text-gray-600">Brand: TOYOTA</p>
+          <p className="text-gray-600">Type: Off road</p>
+          <p className="text-gray-600">Price: $5000</p>
+          <div className="text-left">
+            <Box
+              sx={{
+                "& > legend": { mt: 2 },
+              }}
+            >
+              <Typography component="legend" className="text-white">
+                Rating
+              </Typography>
+              <Rating
+                name="simple-controlled"
+                value={Ratingvalue}
+                onChange={(event, newValue) => {
+                  setRatingvalue(newValue);
+                }}
+              />
+            </Box>
+          </div>
+          <div className="card-actions justify-around my-4">
+            <button className="btn btn-primary bg-[#d54242] hover:bg-[#FF6347] text-white border-0 px-4 py-2 rounded-lg">
+              Details
+            </button>
+            <button className="btn btn-primary bg-[#d54242] hover:bg-[#FF6347] text-white border-0 px-4 py-2 rounded-lg">
+              Update
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
