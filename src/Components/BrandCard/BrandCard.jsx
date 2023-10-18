e8995ed5bdfae86e12897f104f5afe93b81c1c8f
rashidsarkar <rashidrock558@gmail.com>
2023-10-18 13:36:37 +0600

    Enhanced SingleBrand Component for Better UX. Optimized Swiper for seamless image navigation, refined visual layout, and added dynamic data display.

--- src/Components/BrandCard/BrandCard.jsx
+++ src/Components/BrandCard/BrandCard.jsx
@@ -5,7 +5,7 @@ function BrandCard({ item }) {
   const { brandName, brandImage } = item;
 
   return (
-    <Link>
+    <Link to={`/brand/${brandName}`}>
       <div className="block max-w-[18rem] md:max-w-[22rem] lg:max-w-[25rem] mx-auto rounded-lg  shadow-[0 2px 15px -3px rgba(0,0,0,0.07), 0 10px 20px -2px rgba(0,0,0,0.04)] dark:bg-neutral-700 bg-base-300 shadow-xl">
         <div className="relative overflow-hidden bg-cover bg-no-repeat">
           <img className="rounded-t-lg" src={brandImage} alt={brandImage} />
--- src/Pages/SingleBrand/SingleBrand.jsx
+++ src/Pages/SingleBrand/SingleBrand.jsx
@@ -1,5 +1,63 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Autoplay, Navigation } from "swiper/modules";
+import { useParams } from "react-router-dom";
+import "./SingleBrand.css";
+
 function SingleBrand() {
-  return <div>this is towota</div>;
+  const { brandName } = useParams();
+
+  // Replace the sample data with GTR 34 car details
+  const product = {
+    image: "https://i.ibb.co/JddJCCC/pexels-ambady-kolazhikkaran-14061321.jpg", // Image URL of the GTR 34 car
+    name: "GTR 34",
+    brand: "Nissan", // Brand of the car
+    type: "Sports Car", // Type of the car
+    price: 85000.0, // Price of the car
+    rating: 4.8, // Rating of the car
+  };
+
+  return (
+    <div>
+      <p>This is {brandName}</p>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        navigation={true}
+        modules={[Pagination, Autoplay, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img
+            src="https://i.ibb.co/mNSypLZ/summer.jpg"
+            className="md:h-[600px] mx-auto"
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://i.ibb.co/yBBWnTw/offer3.jpg"
+            className="md:h-[600px] mx-auto"
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={product.image} className="md:h-[600px] mx-auto" alt="" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 }
 
 export default SingleBrand;
