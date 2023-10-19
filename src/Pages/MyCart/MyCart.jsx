74f59993cdf5ea5c73856fdb632d239ef9d4fad0
rashidsarkar <rashidrock558@gmail.com>
2023-10-19 16:08:25 +0600

    Improve cart rendering for empty carts

--- src/Pages/MyCart/MyCart.jsx
+++ src/Pages/MyCart/MyCart.jsx
@@ -1,6 +1,6 @@
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import Banar from "../../Components/Banar";
-import { useContext } from "react";
 import { CarsContext } from "../../MainLayout/MainLayout";
 import ItemCard from "./ItemCard";
 import SectionTitle from "../../Components/SectionTitle";
@@ -21,10 +21,18 @@ function MyCart() {
       <div>
         <SectionTitle title={"Browse Your Selection"}></SectionTitle>
 
-        {itemOnCartData.map((item) => (
-          // <Card key={item.id} item={item} onDelete={handleDelete} />
-          <ItemCard key={item._id} item={item}></ItemCard>
-        ))}
+        {itemOnCartData.length === 0 ? (
+          // Display a message when the cart is empty
+          <div className="text-center p-8">
+            <h2 className="text-2xl font-bold">Your cart is empty</h2>
+            <p className="text-gray-600">Add some cars to your cart</p>
+          </div>
+        ) : (
+          // Render cart items
+          itemOnCartData.map((item) => (
+            <ItemCard key={item._id} item={item}></ItemCard>
+          ))
+        )}
       </div>
     </div>
   );
